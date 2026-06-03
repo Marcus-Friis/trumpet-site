44616859a9f8868a65660a42adb4a0a16141e234
Marcus Friis <marcus.friis99@gmail.com>
2026-06-04 01:14:12 +0200

    moved nav to header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,55 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/state';
+	import { routes, type Route } from '$lib/content';
+	import RandomButton from '$lib/RandomButton.svelte';
 
 	let { children } = $props();
+
+	const currentIndex = $derived(routes.findIndex((r) => r.href === page.url.pathname));
+	const prev = $derived(routes[currentIndex - 1] ?? null);
+	const next = $derived(routes[currentIndex + 1] ?? null);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
+{#snippet link(route: Route, direction: 'left' | 'right')}
+	{@const border = direction === 'left' ? 'border-l-2' : 'border-r-2'}
+	<a
+		href={route.href}
+		class="flex h-full w-fit flex-col justify-center gap-1 truncate {border} border-primary px-2 font-semibold not-italic hover:bg-primary hover:text-primary-foreground"
+	>
+		{#if direction === 'left'}
+			← {route.label}
+		{:else if direction === 'right'}
+			{route.label} →
+		{/if}
+	</a>
+{/snippet}
+{#snippet contentNav()}
+	{#if currentIndex !== -1}
+		<nav class="col-span-2 grid w-full grid-cols-3 items-center gap-8">
+			{#if prev}
+				<div class="justify-self-start">{@render link(prev, 'left')}</div>
+			{:else}
+				<span></span>
+			{/if}
+			<div class="justify-self-center">
+				<RandomButton />
+			</div>
+			{#if next}
+				<div class="justify-self-end">{@render link(next, 'right')}</div>
+			{:else}
+				<span></span>
+			{/if}
+		</nav>
+	{/if}
+{/snippet}
+
 <div class="flex min-h-screen flex-col items-center gap-16">
-	<header class="mt-16">
+	<header class="mt-16 w-6/10">
 		<div class="grid grid-cols-3 items-center">
-			<div></div>
 			<h1 class="group text-6xl! font-bold text-primary">
 				<a
 					href="/"
@@ -19,6 +58,7 @@
 					Trumpet
 				</a>
 			</h1>
+			{@render contentNav()}
 		</div>
 	</header>
 
--- src/routes/[content]/+page.svelte
+++ src/routes/[content]/+page.svelte
@@ -1,47 +1,10 @@
 <script lang="ts">
-	import { page } from '$app/state';
-	import { routes, type Route } from '$lib/content';
-	import RandomButton from '$lib/RandomButton.svelte';
 	import type { PageProps } from './$types.ts';
 
 	let { data }: PageProps = $props();
-
-	const currentIndex = $derived(routes.findIndex((r) => r.href === page.url.pathname));
-	const prev = $derived(routes[currentIndex - 1] ?? null);
-	const next = $derived(routes[currentIndex + 1] ?? null);
 </script>
 
-{#snippet link(route: Route, direction: 'left' | 'right')}
-	{@const border = direction === 'left' ? 'border-l-2' : 'border-r-2'}
-	<a
-		href={route.href}
-		class="flex h-full w-fit flex-col justify-center gap-1 truncate {border} border-primary px-2 font-semibold not-italic hover:bg-primary hover:text-primary-foreground"
-	>
-		{#if direction === 'left'}
-			← {route.label}
-		{:else if direction === 'right'}
-			{route.label} →
-		{/if}
-	</a>
-{/snippet}
-
 <div class="flex w-6/10 flex-col items-center gap-4">
-	<nav class="grid w-full grid-cols-3 items-center gap-8">
-		{#if prev}
-			<div class="justify-self-start">{@render link(prev, 'left')}</div>
-		{:else}
-			<span></span>
-		{/if}
-		<div class="justify-self-center">
-			<RandomButton />
-		</div>
-		{#if next}
-			<div class="justify-self-end">{@render link(next, 'right')}</div>
-		{:else}
-			<span></span>
-		{/if}
-	</nav>
-
 	<div class="w-full" style="height: calc(100dvh - var(--layout-offset));">
 		{#if data.contentType === 'pdf'}
 			<embed src={data.content} width="100%" height="100%" type="application/pdf" />
